613f757f364946a504434575472c0c88aaddb3f1
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 14:35:49 +0700

    First commit

---
+++ src/pbo3/pkg10117104/latihan1/PBO310117104Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan1;
+
+/**
+ *
+ * @author 
+ * Nama      : Aditya Suheryana
+ * Kelas     : PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini hanya menampilkan Biodata
+ * 
+ */
+public class PBO310117104Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code aplication logic here
+        System.out.println("BIODATA"+"\n"+"-------");
+        System.out.println("NAMA\t\t: ADITYA SUHERYANA");
+        System.out.println("ALAMAT\t\t: Belakang Jalan Dipatiukur no 120");
+        System.out.println("TEMPATLAHIR\t: MAJALENGKA");
+        System.out.println("Tanggal Lahir\t: 07 November 1998");
+        System.out.println("--------------------------------------------");
+        System.out.println("HOBI\t\t: Main game dan main bola");
+        System.out.println("MAKES\t\t: Soto Ayam");
+        System.out.println("MIKES\t\t: Susu");
+    }
+    
+}
